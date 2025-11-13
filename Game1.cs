1bb605e21c307a64274126953adbe6e661863cdd
339729139 <339729139@gotvdsb.ca>
2025-11-13 08:48:42 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -10,11 +10,11 @@ namespace MonoGame_Topic_3._5___Dragging_and_Dropping
         private SpriteBatch _spriteBatch;
 
 
-        Texture2D carTexture, rocketTexture, asteriodTexture;
-        Rectangle carRect, rocketRect, asteriodRect, window;
+        Texture2D carTexture, rocketTexture, asteroidTexture;
+        Rectangle carRect, rocketRect, asteroidRect, window;
         bool isDraggingCar = false;
         bool isDraggingRocket = false;
-        bool isDraggingAsteriod = false;
+        bool isDraggingAsteroid = false;
         MouseState mouseState, prevMouseState;
         public Game1()
         {
@@ -26,7 +26,7 @@ namespace MonoGame_Topic_3._5___Dragging_and_Dropping
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            asteriodRect = new Rectangle(10, 10, 50, 50);
+            asteroidRect = new Rectangle(10, 10, 50, 50);
             carRect = new Rectangle(200, 200, 75, 25);
             rocketRect = new Rectangle(400, 100, 40, 75);
             window = new Rectangle(0, 0, 800, 500);
@@ -39,7 +39,7 @@ namespace MonoGame_Topic_3._5___Dragging_and_Dropping
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-            asteriodTexture = Content.Load<Texture2D>("Images/asteroid");
+            asteroidTexture = Content.Load<Texture2D>("Images/asteroid");
             carTexture = Content.Load<Texture2D>("Images/fast_car");
             rocketTexture = Content.Load<Texture2D>("Images/rocket");
             // TODO: use this.Content to load your game content here
@@ -52,20 +52,29 @@ namespace MonoGame_Topic_3._5___Dragging_and_Dropping
 
             prevMouseState = mouseState;
             mouseState = Mouse.GetState();
+            if (mouseState.X - prevMouseState.X != 0)
+                this.Window.Title = mouseState.X + " " + prevMouseState.X + " " +(mouseState.X - prevMouseState.X) + "";
 
-            if (NewClick() && asteriodRect.Contains(mouseState.Position))
-                isDraggingAsteriod = true;
-            else if (isDraggingAsteriod && mouseState.LeftButton == ButtonState.Released)
-                isDraggingAsteriod = false;
-            else if (isDraggingAsteriod)
-                asteriodRect.Offset(mouseState.X - prevMouseState.X, mouseState.Y - prevMouseState.Y);
+            if (NewClick() && asteroidRect.Contains(mouseState.Position))
+                isDraggingAsteroid = true;
+            else if (isDraggingAsteroid && mouseState.LeftButton == ButtonState.Released)
+                isDraggingAsteroid = false;
+            else if (isDraggingAsteroid)
+                asteroidRect.Offset(mouseState.X - prevMouseState.X, mouseState.Y - prevMouseState.Y);
 
             if (NewClick() && carRect.Contains(mouseState.Position))
                 isDraggingCar = true;
-            else if (isDraggingAsteriod && mouseState.LeftButton == ButtonState.Released)
+            else if (isDraggingCar && mouseState.LeftButton == ButtonState.Released)
                 isDraggingCar = false;
-            else if (isDraggingAsteriod)
-                asteriodRect.Offset(mouseState.X - prevMouseState.X, 0);
+            else if (isDraggingCar)
+                carRect.Offset(mouseState.X - prevMouseState.X, 0);
+
+            if (NewClick() && rocketRect.Contains(mouseState.Position))
+                isDraggingRocket = true;
+            else if (isDraggingRocket && mouseState.LeftButton == ButtonState.Released)
+                isDraggingRocket = false;
+            else if (isDraggingRocket)
+                rocketRect.Offset(0, mouseState.Y - prevMouseState.Y);
 
             // TODO: Add your update logic here
             base.Update(gameTime);
@@ -77,7 +86,7 @@ namespace MonoGame_Topic_3._5___Dragging_and_Dropping
             _spriteBatch.Begin();
             _spriteBatch.Draw(carTexture, carRect, Color.White);
             _spriteBatch.Draw(rocketTexture, rocketRect, Color.White);
-            _spriteBatch.Draw(asteriodTexture, asteriodRect, Color.White);
+            _spriteBatch.Draw(asteroidTexture, asteroidRect, Color.White);
             _spriteBatch.End();
             // TODO: Add your drawing code here
 
@@ -85,8 +94,7 @@ namespace MonoGame_Topic_3._5___Dragging_and_Dropping
         }
         protected bool NewClick()
         {
-            return mouseState.LeftButton == ButtonState.Pressed && 
-                prevMouseState.LeftButton == ButtonState.Released;
+            return mouseState.LeftButton == ButtonState.Pressed && prevMouseState.LeftButton == ButtonState.Released;
 
         }
 
